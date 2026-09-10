87bb3558f03cb9ef0f8bdb23c2b306bea3a38b48
moririn777 <moriya8229@gmail.com>
2026-09-10 14:04:27 +0900

    Refactor

--- src/components/Belt-output-slider/Belt-output-slider.tsx
+++ src/components/Belt-output-slider/Belt-output-slider.tsx
@@ -1,16 +1,19 @@
-import { Box, Slider, Text} from "@chakra-ui/react";
+import { Box, Slider, Text } from "@chakra-ui/react";
 import { useController } from "../../hooks/useController";
 
 export const BeltoOutputSlider = () => {
-  const {shootPwm, setShootPwm, shootTime, setShootTime} = useController();
+  const { shootPwm, setShootPwm, shootTime, setShootTime } = useController();
   return (
     <Box pt="30px">
       <Text fontSize="sm" fontWeight="bold" mb={8}>
         発射出力 (9~2999)
-        </Text>
-      <Slider.Root width="300px" 
-        defaultValue={[shootPwm]} 
-        min={9} max={2999} step={10}
+      </Text>
+      <Slider.Root
+        width="300px"
+        defaultValue={[shootPwm]}
+        min={9}
+        max={2999}
+        step={10}
         onValueChangeEnd={(e) => setShootPwm(e.value[0])}
       >
         <Slider.Control>
@@ -31,33 +34,33 @@ export const BeltoOutputSlider = () => {
         </Slider.Control>
       </Slider.Root>
 
-        <Text fontSize="sm" fontWeight="bold" mb={8}>
-          発射時間 (s)
-        </Text>
-        <Slider.Root
-          width="300px"
-          defaultValue={[shootTime]}
-          min={0.005}
-          max={0.3}
-          step={0.005}
-          onValueChangeEnd={(e) => setShootTime(e.value[0])}
-        >
-          <Slider.Control>
-            <Slider.Track>
-              <Slider.Range />
-            </Slider.Track>
-            <Slider.Thumb index={0}>
-              <Slider.ValueText
-                position="absolute"
-                bottom="100%"
-                mb="8px"
-                fontSize="sm"
-                fontWeight="bold"
-                whiteSpace="nowrap"
-              />
-            </Slider.Thumb>
-          </Slider.Control>
-        </Slider.Root>
+      <Text fontSize="sm" fontWeight="bold" mb={8}>
+        発射時間 (s)
+      </Text>
+      <Slider.Root
+        width="300px"
+        defaultValue={[shootTime]}
+        min={0.005}
+        max={0.4}
+        step={0.005}
+        onValueChangeEnd={(e) => setShootTime(e.value[0])}
+      >
+        <Slider.Control>
+          <Slider.Track>
+            <Slider.Range />
+          </Slider.Track>
+          <Slider.Thumb index={0}>
+            <Slider.ValueText
+              position="absolute"
+              bottom="100%"
+              mb="8px"
+              fontSize="sm"
+              fontWeight="bold"
+              whiteSpace="nowrap"
+            />
+          </Slider.Thumb>
+        </Slider.Control>
+      </Slider.Root>
     </Box>
   );
 };
