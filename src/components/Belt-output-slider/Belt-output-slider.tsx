0c258ccb9d00fa95e216705cc22765af79633542
moririn777 <moriya8229@gmail.com>
2026-09-11 21:17:20 +0900

    発射

--- src/components/Belt-output-slider/Belt-output-slider.tsx
+++ src/components/Belt-output-slider/Belt-output-slider.tsx
@@ -1,15 +1,25 @@
 import { Box, Slider, Text } from "@chakra-ui/react";
 import { useController } from "../../hooks/useController";
+import "./Belt-output-slider.css";
 
 export const BeltoOutputSlider = () => {
   const { shootPwm, setShootPwm, shootTime, setShootTime } = useController();
+
   return (
-    <Box pt="30px">
-      <Text fontSize="sm" fontWeight="bold" mb={8}>
+    <Box className="shoot-setting">
+      {/* 発射出力 */}
+      <Text
+        className="shoot-setting__label"
+        fontSize="sm"
+        fontWeight="bold"
+        mb={6}
+      >
         発射出力 (9~2999)
       </Text>
+
       <Slider.Root
-        width="300px"
+        className="shoot-slider"
+        width="260px"
         defaultValue={[shootPwm]}
         min={9}
         max={2999}
@@ -34,11 +44,19 @@ export const BeltoOutputSlider = () => {
         </Slider.Control>
       </Slider.Root>
 
-      <Text fontSize="sm" fontWeight="bold" mb={8}>
+      {/* 発射時間 */}
+      <Text
+        className="shoot-setting__label shoot-setting__time"
+        fontSize="sm"
+        fontWeight="bold"
+        mb={6}
+      >
         発射時間 (s)
       </Text>
+
       <Slider.Root
-        width="300px"
+        className="shoot-slider"
+        width="260px"
         defaultValue={[shootTime]}
         min={0.005}
         max={0.4}
@@ -49,6 +67,7 @@ export const BeltoOutputSlider = () => {
           <Slider.Track>
             <Slider.Range />
           </Slider.Track>
+
           <Slider.Thumb index={0}>
             <Slider.ValueText
               position="absolute"
