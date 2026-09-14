ad1d53ed42e03702c7f90993bc7982c92dd991b7
moririn777 <moriya8229@gmail.com>
2026-09-14 13:26:12 +0900

    障害物を追加

--- src/components/konva/konva.tsx
+++ src/components/konva/konva.tsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
-import { Stage, Layer, Image, Line, Arrow } from "react-konva";
+import { Stage, Layer, Image, Line, Arrow, Rect } from "react-konva";
 import useImage from "use-image";
 import Konva from "konva";
 import { useModeStore } from "../../hooks/useController";
 import { ModeTheme, setting } from "../../controller";
 import { useWebSocket } from "../../websocket";
 
-const REAL_FIELD_W = 5700;
-const REAL_FIELD_H = 10500;
+const REAL_FIELD_W = setting.fieldSize.width;
+const REAL_FIELD_H = setting.fieldSize.height;
+
+const NO_ENTRY_ZONES = [
+  { minX: 0, maxX: 450, minY: 0, maxY: 690 },
+  { minX: 1490, maxX: 2000, minY: 2600, maxY: 3310 },
+  { minX: 3820, maxX: 4200, minY: 3800, maxY: 4170 },
+  { minX: 490, maxX: 890, minY: 5590, maxY: 6010 },
+  { minX: 2350, maxX: 2750, minY: 5570, maxY: 6030 },
+  { minX: 4460, maxX: 4740, minY: 5660, maxY: 5940 },
+  { minX: 3820, maxX: 4200, minY: 7430, maxY: 7800 },
+  { minX: 1490, maxX: 2000, minY: 8290, maxY: 9000 },
+];
 
 const SetLocation = () => {
   const { mode } = useModeStore();
@@ -49,22 +60,23 @@ const SetLocation = () => {
       setCurrentPos(pos);
     }
   };
+  const toRealScaleX = REAL_FIELD_W / setting.fieldSizeScale.width;
+  const toRealScaleY = REAL_FIELD_H / setting.fieldSizeScale.height;
+
+  const toScreenScaleX = setting.fieldSizeScale.width / REAL_FIELD_W;
+  const toScreenScaleY = setting.fieldSizeScale.height / REAL_FIELD_H;
 
   const handlePointerUp = () => {
     if (!startPos || !currentPos) return;
 
-    const scaleX = REAL_FIELD_W / setting.fieldSizeScale.width;
-    const scaleY = REAL_FIELD_H / setting.fieldSizeScale.height;
-
-    let realX = startPos.x * scaleX;
-    const realY = startPos.y * scaleY;
+    let realX = startPos.x * toRealScaleX;
+    const realY = REAL_FIELD_H - startPos.y * toRealScaleY;
     let targetDegree = 0;
 
     let dx = currentPos.x - startPos.x;
-    const dy = currentPos.y - startPos.y;
+    const dy = -(currentPos.y - startPos.y);
 
     if (mode === "red") {
-      // X座標と、X方向のベクトルを反転
       realX = REAL_FIELD_W - realX;
       dx = -dx;
     }
@@ -101,6 +113,28 @@ const SetLocation = () => {
           width={setting.fieldSizeScale.width}
           height={setting.fieldSizeScale.height}
         />
+        {NO_ENTRY_ZONES.map((zone, index) => {
+          let x = zone.minX;
+          const w = zone.maxX - zone.minX;
+          const h = zone.maxY - zone.minY;
+
+          // 赤陣地の場合はX座標を反転 (右端の座標を基準にする)
+          if (mode === "red") {
+            x = REAL_FIELD_W - zone.maxX;
+          }
+          const screenY = (REAL_FIELD_H - zone.maxY) * toScreenScaleY;
+          return (
+            <Rect
+              key={index}
+              x={x * toScreenScaleX}
+              y={screenY}
+              width={w * toScreenScaleX}
+              height={h * toScreenScaleY}
+              fill="rgba(52, 49, 34, 0.4)" // 半透明の赤色
+              listening={false} // クリックイベントをブロックしない設定
+            />
+          );
+        })}
 
         <Line
           points={[
