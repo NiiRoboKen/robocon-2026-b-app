8d91c84206c986be4268cc8e4dabe0b8412da7f8
3fuuuu <fuuuu1009@icloud.com>
2026-09-22 20:09:35 +0900

    update

--- src/components/preset/Preset.tsx
+++ src/components/preset/Preset.tsx
@@ -125,79 +125,116 @@ export const Preset = () => {
       </Box>
 
       <Box display="flex" justifyContent="center" alignItems="center" mt={8}>
-        <Grid templateColumns="repeat(3, 1fr)" gap={2} w="300px">
-          <GridItem colStart={2}>
+        <VStack gap={3} w="100%" maxW="240px">
+          {/* 回転 */}
+          <Grid templateColumns="repeat(3, minmax(0, 1fr))" gap={2} w="100%">
             <Button
               w="100%"
-              bg="blue.500"
-              color="white"
-              onClick={() => handleMove("up")}
-            >
-              前
-            </Button>
-          </GridItem>
-
-          <GridItem colStart={1} rowStart={2}>
-            <Button
-              w="100%"
-              bg="blue.500"
-              color="white"
-              onClick={() => handleMove("left")}
-            >
-              左
-            </Button>
-          </GridItem>
-          <GridItem colStart={2} rowStart={3}>
-            <Button
-              w="100%"
-              bg="blue.500"
-              color="white"
-              onClick={() => handleMove("down")}
-            >
-              後
-            </Button>
-          </GridItem>
-          <GridItem colStart={3} rowStart={2}>
-            <Button
-              w="100%"
-              bg="blue.500"
-              color="white"
-              onClick={() => handleMove("right")}
-            >
-              右
-            </Button>
-          </GridItem>
-          <GridItem colStart={2} rowStart={2}>
-            <Button
-              w="100%"
-              bg="red.500"
-              color="white"
-              onClick={() => handleMove("stop")}
-            >
-              停止
-            </Button>
-          </GridItem>
-          <GridItem colStart={1} rowStart={1}>
-            <Button
-              w="100%"
+              h="50px"
               bg="yellow.500"
               color="white"
               onClick={() => handleRotate("ccw")}
+              _active={{
+                transform: "translateY(3px)",
+              }}
             >
               左回転
             </Button>
-          </GridItem>
-          <GridItem colStart={3} rowStart={1}>
+
+            <Box />
+
             <Button
               w="100%"
+              h="50px"
               bg="yellow.500"
               color="white"
               onClick={() => handleRotate("cw")}
+              _active={{
+                transform: "translateY(3px)",
+              }}
             >
               右回転
             </Button>
-          </GridItem>
-        </Grid>
+          </Grid>
+
+          {/* 移動 */}
+          <Grid templateColumns="repeat(3, minmax(0, 1fr))" gap={2} w="100%">
+            <GridItem colStart={2}>
+              <Button
+                w="100%"
+                h="50px"
+                bg="blue.500"
+                color="white"
+                onClick={() => handleMove("up")}
+                _active={{
+                  transform: "translateY(3px)",
+                }}
+              >
+                前
+              </Button>
+            </GridItem>
+
+            <GridItem colStart={1} rowStart={2}>
+              <Button
+                w="100%"
+                h="50px"
+                bg="blue.500"
+                color="white"
+                onClick={() => handleMove("left")}
+                _active={{
+                  transform: "translateY(3px)",
+                }}
+              >
+                左
+              </Button>
+            </GridItem>
+
+            <GridItem colStart={3} rowStart={2}>
+              <Button
+                w="100%"
+                h="50px"
+                bg="blue.500"
+                color="white"
+                onClick={() => handleMove("right")}
+                _active={{
+                  transform: "translateY(3px)",
+                }}
+              >
+                右
+              </Button>
+            </GridItem>
+
+            <GridItem colStart={2} rowStart={3}>
+              <Button
+                w="100%"
+                h="50px"
+                bg="blue.500"
+                color="white"
+                onClick={() => handleMove("down")}
+                _active={{
+                  transform: "translateY(3px)",
+                }}
+              >
+                後
+              </Button>
+            </GridItem>
+
+            <GridItem colStart={2} rowStart={2}>
+              <Button
+                w="100%"
+                h="50px"
+                bg="red.500"
+                color="white"
+                onClick={() => handleMove("stop")}
+                _active={{
+                  transform: "translateY(3px)",
+                }}
+              >
+                停止
+              </Button>
+            </GridItem>
+          </Grid>
+        </VStack>
       </Box>
     </VStack>
   );
