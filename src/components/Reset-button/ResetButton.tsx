49b9362841d1bda7fc84b41270c216479641c678
moririn777 <moriya8229@gmail.com>
2026-09-23 20:56:03 +0900

    feat:リセットの処理を追加

--- src/components/Reset-button/ResetButton.tsx
+++ src/components/Reset-button/ResetButton.tsx
@@ -1,12 +1,35 @@
 import { Button } from "@chakra-ui/react";
+import { useWebSocket } from "../../websocket";
+import { useModeStore } from "../../hooks/useController";
 
 type ResetButtonProps = {
   onReset: () => void;
 };
 
 const ResetButton = ({ onReset }: ResetButtonProps) => {
+  const { sendMessage } = useWebSocket();
+
+  const { mode } = useModeStore();
+
+  // 初期位置に移動
+  const handleResetClick = () => {
+    onReset();
+    sendMessage({
+      command: "navigate",
+      x: 3900,
+      y: 500,
+      degree: 0,
+      theme: mode,
+    });
+  };
+
   return (
-    <Button onClick={onReset} width="100%" background="#F8B400" rounded="3xl">
+    <Button
+      onClick={handleResetClick}
+      width="100%"
+      background="#F8B400"
+      rounded="3xl"
+    >
       Reset Position
     </Button>
   );
